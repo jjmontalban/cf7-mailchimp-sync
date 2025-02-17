586162e3064a607e924a991ed44380c335cab7eb
JJMontalban <jjmontalban@gmail.com>
2025-02-17 09:18:47 +0100

    añadir logs

--- includes/class-mailchimp-api.php
+++ includes/class-mailchimp-api.php
@@ -14,23 +14,24 @@ class CF7_MailChimp_API {
             error_log('MailChimp API Error: Falta la API Key o el ID de la lista.');
             return false;
         }
-    
+
         $dc = explode('-', $this->api_key)[1];
         $url = "https://$dc.api.mailchimp.com/3.0/lists/{$this->list_id}/members/";
-    
+
         $data = [
             'email_address' => $email,
             'status'        => 'subscribed',
             'merge_fields'  => [
                 'FNAME'  => $first_name,
-                'MMERGE4' => $phone,
+                'MMERGE4' => $phone,  
                 'MMERGE3' => $fecha
             ],
             'tags'          => $tags
         ];
-    
-        error_log("MailChimp API - Enviando petición a MailChimp: " . json_encode($data));
-    
+
+        // Agregar log con los datos exactos que se envían
+        error_log("MailChimp API - Enviando datos a MailChimp: " . json_encode($data));
+
         $response = wp_remote_post($url, [
             'body'    => json_encode($data),
             'headers' => [
@@ -38,23 +39,22 @@ class CF7_MailChimp_API {
                 'Content-Type'  => 'application/json'
             ]
         ]);
-        
+
         if (is_wp_error($response)) {
             error_log("MailChimp API - Error en `wp_remote_post()`: " . $response->get_error_message());
             return false;
         }
-        
+
         $response_code = wp_remote_retrieve_response_code($response);
         $response_body = wp_remote_retrieve_body($response);
-        
+
         error_log("MailChimp API - Código de respuesta: $response_code");
         error_log("MailChimp API - Respuesta de MailChimp: " . $response_body);
-        
+
         if ($response_code != 200) {
             error_log('MailChimp API - Error en la solicitud: ' . print_r($response_body, true));
         }
-    
+
         return $response_body;
     }
-    
 }
